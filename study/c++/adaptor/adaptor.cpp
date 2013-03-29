7748e3a06cda6c3202a3fd9953cfbbae9245b81b
risk <risk.masu@gmail.com>
2013-03-29 14:20:49 +0900

    adaptorパターン使ってみた

---
+++ study/c++/adaptor/adaptor.cpp
@@ -0,0 +1,40 @@
+// コンパイル: g++ adaptor.cpp -o adaptor.bin && ./adaptor.bin
+
+#include <iostream>
+
+class TalkInterface
+{
+public:
+
+	virtual void say() = 0;
+};
+
+class Renokun
+{
+public:
+
+	void catSay()
+	{
+		std::cout << "とうちゃん、飯くれよ" << std::endl;
+	}
+};
+
+class RenokunAdaptor
+	: public TalkInterface
+	, private Renokun
+{ 
+public:
+
+	virtual void say()
+	{
+		catSay();
+	}
+};
+
+int main()
+{
+	TalkInterface* interface = new RenokunAdaptor();
+	interface->say();
+	return 0;
+}
+
