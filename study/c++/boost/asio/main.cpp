281cbed671dea43b4cd5a6713ea9883be94efe42
risk <risk.masu@gmail.com>
2013-04-21 23:29:01 +0900

    接続待ち と 接続を作成しました。

--- study/c++/boost/asio/main.cpp
+++ study/c++/boost/asio/main.cpp
@@ -40,6 +40,7 @@ public:
 		th_.join();
 	}
 };
+IoService global_ioService;
 
 void IoService::serviceThread()
 {
@@ -51,19 +52,227 @@ void IoService::serviceThread()
 	ioService_->run();
 }
 
-void print()
+namespace asio = boost::asio;
+using boost::asio::ip::tcp;
+
+class TcpCallback
 {
-	std::cout << "hello asio world." << std::endl;
+private:
+
+	bool accepted_;
+	bool connected_;
+
+public:
+
+	TcpCallback()
+		: accepted_(false)
+		, connected_(false)
+	{
+	}
+
+	virtual bool isAccepted()
+	{
+		return accepted_;
+	}
+	virtual bool isConnected()
+	{
+		return connected_;
+	}
+
+	virtual void onConnect(const boost::system::error_code& error)
+	{
+		std::cout << "onConnect" << std::endl;
+
+		if(error)
+		{
+			std::cout << "connect failed - " << error.message() << std::endl;
+		}
+		else
+		{
+			connected_ = true;
+			std::cout << "connected" << std::endl;
+		}
+	}
+
+	virtual void onAccept(const boost::system::error_code& error)
+	{
+		std::cout << "onAccept" << std::endl;
+
+		if(error)
+		{
+			std::cout << "accept failed - " << error.message() << std::endl;
+		}
+		else
+		{
+			accepted_ = true;
+			std::cout << "accepted" << std::endl;
+		}
+	}
+};
+
+typedef boost::shared_ptr<TcpCallback> TcpCallback_ptr;
+typedef boost::shared_ptr<tcp::acceptor> Acceptor_ptr;
+class TcpSocket
+{
+private:
+
+	tcp::socket socket_;
+	TcpCallback_ptr callback_;
+	Acceptor_ptr acceptor_;
+
+	std::string ip_;
+	int port_;
+
+public:
+
+	TcpSocket(const int port, TcpCallback_ptr callback = TcpCallback_ptr())
+		: socket_(global_ioService.get())
+		, callback_(callback)
+		, port_(port)
+	{
+		// コールバックが存在しない場合は、デフォルトを詰めておく
+		if(!callback_)
+		{
+			callback_ = boost::make_shared<TcpCallback>();
+		}
+
+		acceptor_ = boost::shared_ptr<tcp::acceptor>(
+			new tcp::acceptor(
+				global_ioService.get(),
+				tcp::endpoint(tcp::v4(), port_)));
+	}
+	
+	TcpSocket(
+		const std::string& ip,
+		const int port,
+		TcpCallback_ptr callback = TcpCallback_ptr())
+			: socket_(global_ioService.get())
+			, callback_(callback)
+			, ip_(ip)
+			, port_(port)
+	{
+		// コールバックが存在しない場合は、デフォルトを詰めておく
+		if(!callback_)
+		{
+			callback_ = boost::make_shared<TcpCallback>();
+		}
+	}
+
+	virtual bool accept()
+	{
+		boost::system::error_code error;
+		acceptor_->accept(socket_, error);
+
+		if(error)
+		{
+			std::cout << "accept failed - " << error.message() << std::endl;
+			return false;
+		}
+
+		// accepted
+		return true;
+	}
+
+	virtual void acceptAsync()
+	{
+		acceptor_->async_accept(
+			socket_,
+			boost::bind(&TcpCallback::onAccept, callback_, asio::placeholders::error));
+	}
+
+	virtual bool connect()
+	{
+		boost::system::error_code error;
+		socket_.connect(
+			tcp::endpoint(asio::ip::address::from_string(ip_), port_),
+			error);
+		if(error)
+		{
+			std::cout << "connect failed - " << error.message() << std::endl;
+			return false;
+		}
+		// connected
+		return true;
+	}
+
+	virtual void connectAsync()
+	{
+		socket_.async_connect(
+			tcp::endpoint(asio::ip::address::from_string(ip_), port_),
+			boost::bind(&TcpCallback::onConnect, callback_, asio::placeholders::error));
+	}
+
+};
+
+
+void serverThread()
+{ 
+	TcpSocket socket(40000);
+	if(socket.accept())
+	{
+		std::cout << "server thread accepted" << std::endl;
+	}
+	else
+	{
+		std::cout << "server thread accept failed" << std::endl;
+	}
 }
 
+void asyncServerThread()
+{
+	TcpCallback_ptr callback = boost::make_shared<TcpCallback>();
+	TcpSocket socket(40000, callback);
+	socket.acceptAsync();
+
+	while(!callback->isAccepted())
+	{
+		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+	}
+}
+
+void clientThread()
+{
+	TcpSocket socket("127.0.0.1", 40000);
+	if(socket.connect())
+	{
+		std::cout << "client thread connected" << std::endl;
+	}
+	else
+	{
+		std::cout << "client thread connect failed" << std::endl;
+	}
+}
+
+void asyncClientThread()
+{ 
+	TcpCallback_ptr callback = boost::make_shared<TcpCallback>();
+	TcpSocket socket("127.0.0.1", 40000, callback);
+	socket.connectAsync();
+
+	while(!callback->isConnected())
+	{
+		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	IoService ioService;
+	std::cout << "async server start" << std::endl;
+//	boost::thread server(boost::bind(serverThread));
+	boost::thread server(boost::bind(asyncServerThread));
+	std::cout << "async server start OK" << std::endl;
 
-	std::cout << boost::format("hello %d") % "world" << std::endl;
+	std::cout << "sleep in" << std::endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	std::cout << "sleep out" << std::endl;
 
-	ioService.get().post( print );
+	std::cout << "async client start" << std::endl;
+//	boost::thread client(boost::bind(clientThread));
+	boost::thread client(boost::bind(asyncClientThread));
+	std::cout << "async client start OK" << std::endl;
+
+	client.join();
+	server.join();
 
 	return 0;
 } 
