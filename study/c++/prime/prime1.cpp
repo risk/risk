51ca4300701c4a95601caaf57228510585755bb6
risk <risk.masu@gmail.com>
2013-08-15 14:28:06 +0900

    result : 269912 usec のコードを、別名保存

---
+++ study/c++/prime/prime1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+#include <boost/format.hpp>
+#include <sys/time.h>
+
+int main()
+{
+	struct timeval start, end;
+	std::list<int> prime;
+
+	gettimeofday(&start, NULL);
+
+	for(int i = 1; i < 10000; ++i) {
+		bool divided = false;
+		for(int j = 2; j < i; ++j) {
+			if( i % j == 0) {
+				divided = true;
+			}
+		}
+		if(!divided) {
+			prime.push_back(i);
+		}
+	}
+
+	gettimeofday(&end, NULL);
+
+	int result = ((end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec) - start.tv_usec;
+	std::cout
+		<< boost::format("result : %d usec") % result
+		<< std::endl;
+
+	return 0;
+}
